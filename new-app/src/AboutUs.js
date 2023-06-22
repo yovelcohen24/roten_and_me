c996b7f09cabc4a0143df5dc976ff3a555545f4a
yovel cohen <109220945+yovelcohen24@users.noreply.github.com>
2023-06-22 21:18:14 +0300

    Update AboutUs.js
    NUFI

--- new-app/src/AboutUs.js
+++ new-app/src/AboutUs.js
@@ -6,7 +6,7 @@ const AboutUsPage = () => {
       <h1>About Us</h1>
       <p>This is a small family business offering zimmer rentals. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus ligula vitae mauris tincidunt, eget tempor nisi venenatis. Vivamus auctor semper ante, id fringilla lectus aliquet nec. Sed tincidunt nunc eget dolor aliquam, ac volutpat nisl commodo.</p>
     </div>
-  );
+  );//
 };
 
 export default AboutUsPage;
