31578615135f6eeb584b3e50d2d3ab3577f951a8
rotemlv <90067343+rotemlv@users.noreply.github.com>
2023-06-25 19:59:50 +0300

    changed language to english, hebrew does not work

--- new-app/src/HomePage.js
+++ new-app/src/HomePage.js
@@ -65,7 +65,9 @@ import './homepage.css';
     };
     const backgroundImage = "sitew.jpg";
     return (
-      <div className="bg-gray-200 bg-opacity-50 centered-cut-corner-wrapper bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url(/${backgroundImage})` }}>
+      
+    <div className="bg-gray-200 bg-opacity-50 centered-cut-corner-wrapper bg-no-repeat bg-center bg-cover" style={{ backgroundImage: `url(/${backgroundImage})` }}>
+      <div className="container mx-auto px-4 bg-gray-100 min-h-screen bg-opacity-80 transition-colors duration-500 ease-in-out hover:bg-opacity-90">
       <div className="container mx-auto text-center">
         
       <nav className="flex items-center justify-center py-4">
@@ -90,7 +92,7 @@ import './homepage.css';
 
       
       <div id="general" className="py-8">
-        <h1 className="text-4xl font-bold">ברוכים הבאים לצימרים בר יוחאי</h1>
+        <h1 className="text-4xl font-bold pb-6">Welcome to Bar-Yohai lodging!</h1>
         <div className="max-w-screen-lg mx-auto h-0 pb-[30%] overflow-hidden relative flex items-center justify-center">
           <img src={bnbImages[currentImageIndex]} alt={`B&B ${currentImageIndex + 1}`} className="object-cover absolute inset-0 w-full h-full" />
         </div>
@@ -111,7 +113,7 @@ import './homepage.css';
         </div>
       </div>
       <div id="opinions" className="py-8">
-  <h2 className="text-2xl font-bold mb-4">המלצות מאורחים</h2>
+  <h2 className="text-2xl font-bold mb-4">Guest Recommendations</h2>
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
     {testimonialsData.map((testimonial, index) => (
       <TestimonialItem key={index} testimonial={testimonial} />
@@ -120,7 +122,7 @@ import './homepage.css';
 </div>
 
 <div id="specials" className="py-8 flex flex-col items-center">
-      <h2 className="text-2xl font-bold mb-4">המבצעים שלנו</h2>
+      <h2 className="text-2xl font-bold mb-4">Our Promotions</h2>
       <div className="promotions_container mx-auto">
         <div className="max-w-lg mx-auto">
           {promotionsData.map((promotion, index) => (
@@ -147,7 +149,7 @@ import './homepage.css';
       </div>
       <div className="room_button_container mt-4">
         <button className="room_button" onClick={goToRoomPage}>
-          כנס למידע על החדרים עצמם ושריין חדר בהקדם
+              Click to save a room!
         </button>
       </div>
     </div>
@@ -155,7 +157,7 @@ import './homepage.css';
 
 
 <div id="around" className="py-8">
-  <h2 className="text-2xl font-bold mb-4">מה יש בסביבה</h2>
+  <h2 className="text-2xl font-bold mb-4">What's around us</h2>
   <div className="places-carousel relative mx-auto">
     <button className="carousel-arrow previous absolute top-1/2 left-0 transform -translate-y-1/2" onClick={handlePreviousPlace}>
       &lt;
@@ -174,7 +176,7 @@ import './homepage.css';
         <h3 className="text-lg font-bold">{placesData[currentPlaceIndex].name}</h3>
         <p className="mt-2">{placesData[currentPlaceIndex].description}</p>
         <a href={placesData[currentPlaceIndex].website} target="_blank" rel="noopener noreferrer" className="text-blue-500 mt-2 inline-block">
-          בקר באתר
+          Visit this website
         </a>
       </div>
     </div>
@@ -184,17 +186,18 @@ import './homepage.css';
   </div>
       
   <div id="how-to-get" className="py-8">
-  <h2 className="text-2xl font-bold mb-4">מפת הגעה</h2>
+  <h2 className="text-2xl font-bold mb-4">Map</h2>
   <div className="map_wrapper">
     <a href="https://goo.gl/maps/XmfbkpVFydZoXFVG8" target="_blank" rel="noopener noreferrer" className="map_link">
       <img src={require('./homepage_pictures/map_image.jpg')} alt="Map" className="map_image" />
-      <span className="map_text">איך להגיע אלינו</span>
+      <span className="map_text">How to reach us</span>
     </a>
   </div>
 </div>
 
 
     </div>
+  </div>
   </div>
   </div>
 </div>
